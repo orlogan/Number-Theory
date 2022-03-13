1458cc379d4309f6bce1ec8c26d93b7396d0e5de
orlogan <orlogan7@gmail.com>
2022-03-13 08:26:41 -0400

    Set up External Lib's and Created Matplotlib test

--- src/core.py
+++ src/core.py
@@ -3,18 +3,16 @@
 
 from . import helpers
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def add_one(number):
     """Adds one to number"""
     return number + 1
 
+def basicPlot():
+    """Shows a parabola"""
+    plt.plot([1,2,3,4])
+    plt.ylabel('some numbers')
+    plt.show()
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
-
-
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,6 +11,8 @@ class BasicTestSuite(unittest.TestCase):
     def test_absolute_truth_and_meaning(self):
         assert True
 
+    def test_plot(self):
+        assert False
 
 if __name__ == '__main__':
     unittest.main()
